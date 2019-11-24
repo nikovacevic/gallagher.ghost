61609370e60de8c33e476146f79c945ee2e44409
Niko Kovacevic <nikovacevic@gmail.com>
2019-11-23 20:24:22 -0700

    Style updates and Ghost 3.0 updates

--- assets/js/gallagher.js
+++ assets/js/gallagher.js
@@ -3,41 +3,24 @@ $(function () {
   UI.viewportHeight = $(window).height();
   UI.viewportWidth = $(window).width();
   UI.fullScreenNav = $("nav#full-screen-nav");
-  UI.headerNav = $("header>nav");
 
   UI.init = function () {
     // Set ATF image
-    $("section.atf").css('height', UI.viewportHeight-$('header').height());
-    $("section.atf").css('margin-top', $('header').height());
+    $("section.atf").css('height', UI.viewportHeight);
 
     // Set fixed images
     $("section.fixed-image").css('height', UI.viewportHeight*0.45);
     setTimeout(function () {
       $("body").removeClass('loading');
     }, 250)
-
-    // Remove loading state
-    setTimeout(function () {
-      $("main, body").removeClass('loading');
-    }, 200)
   }
 
   // Full-screen nav toggle
   UI.toggleFullScreenNav = function (nav) {
     nav.toggleClass('visible');
-    $("header").toggleClass('scrolling', !nav.hasClass('visible'));
   }
   $('.toggle-menu').click(function (e, ui) {
     UI.toggleFullScreenNav(UI.fullScreenNav);
-  });
-
-  // Set nav scrolling state
-  $(window).scroll(function (e, ui) {
-    if ($(document).scrollTop() > 30) {
-      $('header').addClass('scrolling');
-    } else {
-      $('header').removeClass('scrolling');
-    }
   });
 
   // Collapse full-screen nav on navigation
